2bbd804033f547089833818ab74dbf43c79711f8
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2025-01-10 20:01:21 +0530

    Initial donation routes

---
+++ src/controllers/donations.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export const StartDonationHandler = async (req: Request, res: Response) => {
+
+}
+
+export const ApproveDonationHandler = async (req: Request, res: Response) => {
+
+}
+
+export const ConfirmDonationHandler = async (req: Request, res: Response) => {
+
+}
--- src/routes/donations.ts
+++ src/routes/donations.ts
@@ -1,5 +1,11 @@
+import { authMiddleware } from "@src/middleware/token";
 import { Router } from "express";
+import { StartDonationHandler, ApproveDonationHandler, ConfirmDonationHandler } from "@src/controllers/donations";
 
 const router = Router();
+
+router.post("/food", authMiddleware, StartDonationHandler);
+router.post("/approve/:donationId", authMiddleware, ApproveDonationHandler);
+router.post("/confirm/:donationId", authMiddleware, ConfirmDonationHandler);
 
 export { router as DonationsRouter };
