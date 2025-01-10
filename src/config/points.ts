8194af884826c8912e1faa07790fe9845e966d56
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2025-01-10 20:54:43 +0530

    Add: Points map

---
+++ src/config/points.ts
@@ -0,0 +1,20 @@
+export const pointMap = {
+  // Difference point brackets based on size
+  donation: {
+    loaf: 100,
+    prince: 200,
+    king: 300
+  },
+  // Points for referring the app to friends
+  referral: 50,
+  // Points given for check-in
+  checkIn: 5,
+  // Points given from new-login
+  newRecruit: 20,
+  // Points for three day streak
+  threeDayStreak: 25,
+  // Points for weekly streak
+  weeklyStreak: 50,
+  // Points for monthly streak
+  monthlyStreak: 100,
+}
